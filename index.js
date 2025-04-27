16075c19c0093a7d3a318c03b9b804316bbaaa74
CuneytAkturk1903 <cuneytakturk1903@gmail.com>
2025-04-28 02:34:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const WebSocket = require('ws');
-
 const server = new WebSocket.Server({ port: 3000 });
 
 server.on('connection', socket => {
+    console.log("Yeni kullanıcı bağlandı!");
+
     socket.on('message', message => {
-        // Gelen mesajı herkese gönder
-        server.clients.forEach(client => {
-            if (client !== socket && client.readyState === WebSocket.OPEN) {
-                client.send(message);
+        if (typeof message === "string") {
+            const data = JSON.parse(message);
+            if (data.type === "text") {
+                // Yazılı mesajsa
+                server.clients.forEach(client => {
+                    if (client !== socket && client.readyState === WebSocket.OPEN) {
+                        client.send(`${data.message}`);
+                    }
+                });
             }
-        });
+        } else {
+            // Ses verisiyse
+            server.clients.forEach(client => {
+                if (client !== socket && client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                }
+            });
+        }
     });
 });
 
-console.log('WebSocket Server çalışıyor Port: 3000');
+console.log("WebSocket sunucusu 3000 portunda çalışıyor!");
